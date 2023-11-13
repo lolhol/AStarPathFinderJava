201154149b1a44add111416bb44682a346727a86
brigerodev <62488289+lolhol@users.noreply.github.com>
2023-11-13 14:39:37 -0800

    improvements

--- src/main/java/star/finder/main/AStar.java
+++ src/main/java/star/finder/main/AStar.java
@@ -6,11 +6,19 @@ import star.finder.util.Node;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.PriorityQueue;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 // A basic AStar module
 public class AStar {
-    private final int[][] DIRECTIONS = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-    public Node run(Node start, Node end, boolean[][] obstacleGrid, int maxIter) {
+    // Possibly soon multithreading but idk
+
+    private final int[][] DIRECTIONS = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
+
+    public Node run(Node start, Node end, boolean[][] obstacleGrid, int maxIter, boolean isMulti) {
         PriorityQueue<Node> openList = new PriorityQueue<>();
         boolean[] isInOpenList = new boolean[obstacleGrid.length * obstacleGrid[0].length];
         boolean[] closedList = new boolean[obstacleGrid.length * obstacleGrid[0].length];
@@ -23,7 +31,6 @@ public class AStar {
             isInOpenList[currentNode.x * obstacleGrid[0].length + currentNode.y] = false;
 
             if (currentNode.x == end.x && currentNode.y == end.y) {
-                //System.out.println(cur);
                 return currentNode;
             }
 
@@ -36,8 +43,8 @@ public class AStar {
                         obstacleGrid[newX][newY] &&
                         !closedList[newX * obstacleGrid[0].length + newY]) {
 
-                    double newH = MathUtil.getDistance(currentNode, end) + currentNode.getNonAirAround(3, 3, obstacleGrid);
-                    //if (!isInOpenList[newX * obstacleGrid[0].length + newY] || newH < currentNode.h) {
+                    double newH = MathUtil.getDistance(currentNode, end)
+                            + currentNode.getNonAirAround(3, 3, obstacleGrid);
                     double newAddR = MathUtil.getDistance(currentNode, start);
                     Node neighbor = new Node(newX, newY, currentNode, newAddR, newH);
                     openList.add(neighbor);
@@ -52,7 +59,7 @@ public class AStar {
         return null; // No path found
     }
 
-    public Node run(int[] pos1, int[] pos2, List<List<Integer>> nums) {
+    public Node run(int[] pos1, int[] pos2, List<List<Integer>> nums, int maxIter) {
         boolean[][] booleanList = new boolean[nums.size()][nums.get(0).size()];
 
         for (int x = 0; x < nums.size(); ++x) {
@@ -62,11 +69,12 @@ public class AStar {
             }
         }
 
-        return run(new Node(pos1[0], pos1[1], null, 0, MathUtil.getDistance(pos1, pos2)), new Node(pos2[0], pos2[1], null, MathUtil.getDistance(pos1, pos2), 0), booleanList, 10000);
+        return run(new Node(pos1[0], pos1[1], null, 0, MathUtil.getDistance(pos1, pos2)),
+                new Node(pos2[0], pos2[1], null, MathUtil.getDistance(pos1, pos2), 0), booleanList, maxIter, false);
     }
 
     public List<Node> run(int[] pos1, int[] pos2, List<List<Integer>> nums, boolean isList) {
-        Node result = run(pos1, pos2, nums);
+        Node result = run(pos1, pos2, nums, 10000);
         if (result == null) {
             return new ArrayList<>();
         }
